8a85bdfef75868b411c596c3e48541959a0f4b45
Kisha Richardson <kisha@spunklabs.com>
2016-05-09 18:05:18 -0700

    add lesson_5

---
+++ 05_Ruby_Fundamentals/exercises/get_it.rb
@@ -0,0 +1,25 @@
+#1 - Pull the json from the reddit API via
+  # http://www.reddit.com/.json
+  # http://mashable.com/stories.json
+#2 - Parse it using the JSON library
+#3 - Find the stories based on techniques used in the code_along (max of 25 provided)
+#4 - Create a new story hash out of each story with the following keys :title, :upvotes and :category
+    #title, #category and #upvotes may not be the names use
+#5 - Add each story to an array
+#6 - Print each story from the array on your "Front Page"
+#7 - BONUS ### create an 'csv' file using the api data & Ruby's CSV library
+     # -> http://ruby-doc.org/stdlib-2.2.2/libdoc/csv/rdoc/CSV.html
+         #title, upvote, category are the required columns
+
+require 'typhoeus'#library that faciltates http requests
+require 'pry'
+require 'pry-byebug'
+require 'json'
+
+def connect_to_api(url)
+  response = Typhoeus.get(url)
+  JSON.parse(response.body)
+end
+
+reddit_url ='http://www.reddit.com/.json'
+reddit_json_response = connect_to_api(reddit_url)
